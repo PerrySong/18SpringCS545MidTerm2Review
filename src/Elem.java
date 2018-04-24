861ebd024315e26859e08d0d0965afe4a001e5c3
spfscmz <psong4@dons.usfca.edu>
2018-04-24 12:36:31 -0700

    This is Sorting Algorithms

---
+++ src/Elem.java
@@ -0,0 +1,21 @@
+public class Elem {
+    private int key;
+    private Object data;
+
+    public Elem(int key, Object data) {
+        this.key = key;
+        this.data = data;
+    }
+
+    public int key() {
+        return key;
+    }
+
+    public Object data() {
+        return data;
+    }
+
+    public String toString() {
+        return key + " " + data + "; ";
+    }
+}
